bf1d64d699a52bffe8f9811a5bceb70d331c6b21
Jerry Liu <38052342+liujiaruirz@users.noreply.github.com>
2020-04-03 17:12:39 -0700

    LevelSelection Scene Added
    Added LevelSelection Scene as well as the function of going to the backward scene (hit the back button to go to StartMenu). Added MiddleAge style background.

---
+++ Assets/LevelSelect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    private int preScene;
+    private void Start()
+    {
+        preScene = SceneManager.GetActiveScene().buildIndex - 1;
+
+    }
+    public void GoBack()
+    {
+        SceneManager.LoadScene(preScene);
+    }
+
+
+}
