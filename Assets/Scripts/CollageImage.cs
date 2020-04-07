82e55ecf52d1a8f711afec72c397ea67cb448309
yizhouzhao <yizhou_zhao@berkeley.edu>
2020-04-07 16:01:53 -0700

    resized image

--- Assets/Scripts/CollageImage.cs
+++ Assets/Scripts/CollageImage.cs
@@ -25,11 +25,37 @@ public class CollageImage : MonoBehaviour
     public void LoadSprite(string fileName)
     {
         print("Collage image load sprite");
-        var texture = Resources.Load<Texture2D>(fileName);
+        Texture2D texture = Resources.Load<Texture2D>(fileName);
         print(texture.width);
         print(texture.height);
-        PaintingSpriteRenderer.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-    } 
+
+        //cut the image into square size
+        int newTextureSize = Mathf.Min(texture.width, texture.height);
+        Texture2D destTex = new Texture2D(newTextureSize, newTextureSize);
+        Color[] pix = texture.GetPixels(0, 0, texture.width, texture.width);
+        destTex.SetPixels(pix);
+        destTex.Apply();
+
+        Texture2D resizedTex = ScaleTexture(destTex, 400, 400);
+
+        PaintingSpriteRenderer.sprite = Sprite.Create(resizedTex, new Rect(0, 0, 400, 400), new Vector2(0.5f, 0.5f));
+    }
+
+
+    private Texture2D ScaleTexture(Texture2D source, int targetWidth, int targetHeight)
+    {
+        Texture2D result = new Texture2D(targetWidth, targetHeight, source.format, true);
+        Color[] rpixels = result.GetPixels(0);
+        float incX = (1.0f / (float)targetWidth);
+        float incY = (1.0f / (float)targetHeight);
+        for (int px = 0; px < rpixels.Length; px++)
+        {
+            rpixels[px] = source.GetPixelBilinear(incX * ((float)px % targetWidth), incY * ((float)Mathf.Floor(px / targetWidth)));
+        }
+        result.SetPixels(rpixels, 0);
+        result.Apply();
+        return result;
+    }
 
 
 }
