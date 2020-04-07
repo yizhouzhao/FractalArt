6df0f45527fa83717703acc75b8b42a8b9000dac
yizhouzhao <yizhou_zhao@berkeley.edu>
2020-04-07 14:31:22 -0700

    add shapemove

---
+++ Assets/Scripts/CollageFraction.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollageFraction : MonoBehaviour
+{
+
+    //Move by left button
+    private Vector3 mOffset;
+    private float mZCoord;
+    private Vector3 currentPosition;
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void OnMouseDrag()
+    {
+        //if (EventSystem.current.IsPointerOverGameObject())
+        //{
+        //    return;
+        //}
+        transform.position = GetMouseWorldPos() + mOffset;
+
+
+        //this.transform.position = GetGridPosition(transform.position);
+    }
+
+    private void OnMouseOver()
+    {
+        //print("on mouse over");
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+        currentPosition = this.transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
