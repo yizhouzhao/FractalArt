9e16c6af1539591e2a6d4ff0f510735d432d3b43
Jerry Liu <38052342+liujiaruirz@users.noreply.github.com>
2020-04-09 16:30:05 -0700

    Added AssetBundle

---
+++ Assets/Scripts/Editor/BuildAssetBundle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class BuildAssetBundle : Editor
+{
+    [MenuItem("Assets/Build AssetBundle")]
+
+    static void BuildAllAssetBundles()
+    {
+        Debug.Log(Application.persistentDataPath);
+        BuildPipeline.BuildAssetBundles(@Application.persistentDataPath, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.iOS);
+    }
+}
---
+++ Assets/Scripts/LoadAssetBundles.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAssetBundles : MonoBehaviour
+{
+
+    AssetBundle myLoadedAssetbundle;
+    public string path;
+    public int pictureIndex;
+
+    void Start()
+    {
+        LoadAssetBundle(path);
+        CreateObjectFrombundle(pictureIndex.ToString());
+    }
+
+    void LoadAssetBundle(string bundleUrl)
+    {
+        myLoadedAssetbundle = AssetBundle.LoadFromFile(bundleUrl); //could be anything: server, path, etc.
+        Debug.Log(myLoadedAssetbundle == null ? "Failed to load AssetBundle" : "Successfully loaded AssetBundle");
+    }
+
+    void CreateObjectFrombundle(string assetName)
+    {
+        var prefab = myLoadedAssetbundle.LoadAsset(assetName);
+        Instantiate(prefab);
+    }
+
+
+    public void ToggleLeft()
+    {
+        if (pictureIndex == 1)
+        {
+            pictureIndex = 1;
+        }
+        else
+        {
+            Object.Destroy(pictureIndex.ToString());
+            pictureIndex -= 1;
+            CreateObjectFrombundle(pictureIndex.ToString());
+        }
+    }
+
+    public void ToggleRight()
+    {
+        if (pictureIndex == 4)
+        {
+            pictureIndex = 4;
+        }
+        else
+        {
+            Object.Destroy(pictureIndex);
+            pictureIndex += 1;
+            CreateObjectFrombundle(pictureIndex.ToString());
+        }
+    }
+
+    public void toGameScene()
+    {
+        SceneManager.LoadScene("GameScene2222");
+    }
+}
+
