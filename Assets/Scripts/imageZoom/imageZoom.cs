b151d453ca26c557deb71b5e98db430d01779332
Jerry Liu <38052342+liujiaruirz@users.noreply.github.com>
2020-04-11 19:58:44 -0700

    Zoom-in Added
    Able to Zoom in images

---
+++ Assets/Scripts/imageZoom/imageZoom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class imageZoom : MonoBehaviour
+{
+    public ImageZoomExplorer imageZoomExplorerObject;
+
+    public void OnClickButton()
+    {
+        Sprite TargetSprite;
+        TargetSprite = PlayerInfo.loadedAssetBundle.LoadAsset<Sprite>(PlayerInfo.pictureIndex.ToString());
+
+        imageZoomExplorerObject.OpenImageExplorer(TargetSprite);
+    }
+}
